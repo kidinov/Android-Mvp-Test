82e90837d81c98c84f12cb456324d0998cbf04dd
Ivan Carballo <ivan@ribot.co.uk>
2015-12-08 10:01:44 +0000

    Implement RxSchedulersOverrideRule

--- app/src/test/java/uk/co/ribot/androidboilerplate/MainPresenterTest.java
+++ app/src/test/java/uk/co/ribot/androidboilerplate/MainPresenterTest.java
@@ -2,9 +2,11 @@ package uk.co.ribot.androidboilerplate;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.Collections;
 import java.util.List;
@@ -12,22 +14,25 @@ import java.util.List;
 import rx.Observable;
 import uk.co.ribot.androidboilerplate.data.DataManager;
 import uk.co.ribot.androidboilerplate.data.model.Ribot;
-import uk.co.ribot.androidboilerplate.runner.RxMockitoJUnitRunner;
 import uk.co.ribot.androidboilerplate.test.common.TestDataFactory;
 import uk.co.ribot.androidboilerplate.ui.main.MainMvpView;
 import uk.co.ribot.androidboilerplate.ui.main.MainPresenter;
+import uk.co.ribot.androidboilerplate.util.RxSchedulersOverrideRule;
 
 import static org.mockito.Matchers.anyListOf;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
-@RunWith(RxMockitoJUnitRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 public class MainPresenterTest {
 
     @Mock MainMvpView mMockMainMvpView;
     @Mock DataManager mMockDataManager;
     private MainPresenter mMainPresenter;
+
+    @Rule
+    public final RxSchedulersOverrideRule mOverrideSchedulersRule = new RxSchedulersOverrideRule();
 
     @Before
     public void setUp() {
