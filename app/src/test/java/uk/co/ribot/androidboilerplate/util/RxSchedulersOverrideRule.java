6df6806c1832cceeb214ff4e11a167c6523c109a
Plastix <Plastix@users.noreply.github.com>
2016-05-05 18:42:58 -0400

    Update RxJava to 1.1.4 and remove RxSchedulersOverrideRule hack (#37)
    This removes the reflection used in RxSchedulersOverrideRule to call
    RxJavaPlugin's reset() method since this method is now public in version
    1.1.4

--- app/src/test/java/uk/co/ribot/androidboilerplate/util/RxSchedulersOverrideRule.java
+++ app/src/test/java/uk/co/ribot/androidboilerplate/util/RxSchedulersOverrideRule.java
@@ -4,9 +4,6 @@ import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 import rx.Scheduler;
 import rx.android.plugins.RxAndroidPlugins;
 import rx.android.plugins.RxAndroidSchedulersHook;
@@ -41,16 +38,6 @@ public class RxSchedulersOverrideRule implements TestRule {
         }
     };
 
-    // Hack to get around RxJavaPlugins.reset() not being public
-    // See https://github.com/ReactiveX/RxJava/issues/2297
-    // Hopefully the method will be public in new releases of RxAndroid and we can remove the hack.
-    private void callResetViaReflectionIn(RxJavaPlugins rxJavaPlugins)
-            throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
-        Method method = rxJavaPlugins.getClass().getDeclaredMethod("reset");
-        method.setAccessible(true);
-        method.invoke(rxJavaPlugins);
-    }
-
     @Override
     public Statement apply(final Statement base, Description description) {
         return new Statement() {
@@ -58,13 +45,14 @@ public class RxSchedulersOverrideRule implements TestRule {
             public void evaluate() throws Throwable {
                 RxAndroidPlugins.getInstance().reset();
                 RxAndroidPlugins.getInstance().registerSchedulersHook(mRxAndroidSchedulersHook);
-                callResetViaReflectionIn(RxJavaPlugins.getInstance());
+
+                RxJavaPlugins.getInstance().reset();
                 RxJavaPlugins.getInstance().registerSchedulersHook(mRxJavaSchedulersHook);
 
                 base.evaluate();
 
                 RxAndroidPlugins.getInstance().reset();
-                callResetViaReflectionIn(RxJavaPlugins.getInstance());
+                RxJavaPlugins.getInstance().reset();
             }
         };
     }
